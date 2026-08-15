27694fd5150a1d6a56968453aa31f3cdabf80dc2
Girma Halie <girma.m.halie19@gmail.com>
2026-08-15 11:38:52 -0500

    Initial AGESStore app scaffold

---
+++ App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>This is for the Store/Admin app part a part of the AGES project.</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
